2b65a2b9659002b133052e74d6f778d91e9e5201
arshadwebdeveloper10 <arshadwebdeveloper10@gmail.com>
2024-10-16 19:08:57 +0000

    added the register and login form

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const navigation = [
 
 const Navbar = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const currentUser = true;
+    const currentUser = false;
   return (
     <header className="max-w-screen-2xl mx-auto px-6 py-4">
         <nav className="flex justify-between items-center">
--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,11 +1,15 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import App from "../App";
 import HomePage from "../pages/home/Home";
+import Register from '../components/Register'
+import Login from '../components/Login'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<App />}>
         <Route index element={<HomePage/>} />
+        <Route path="register" element={<Register/>} />
+        <Route path="login" element={<Login/>} />
       </Route>
     )
   );
